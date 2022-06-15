924250fb4325ba3b63da005156ded7e14cf5d95d
cauan <cauandesouza07@gmail.com>
2022-06-15 07:40:36 -0300

    alteraçao

--- 2.banco de dados/dml.sql
+++ 2.banco de dados/dml.sql
@@ -1,7 +1,7 @@
 use site;
 
 insert into tb_usuario(nm_usuario, ds_telefone ,ds_email , ds_senha )
-		values(  'adm','11 957478957', 'admin@admin.com', '12345' );
+		values(  'adm','11 957478957', 'admin@gmail.com', '1234' );
         
         
 -- CSU01:: efetuar login
@@ -10,27 +10,28 @@ select id_usuario 		id,
        ds_email			email,
        img_usuario
   from tb_usuario;
- where ds_email 		= 'kaiofdss@gmail.com'
+ where ds_email 		= 'admin@gmail.com'
    and ds_senha			= '1234';
      
      
+-- CSU01:: efetuar anuncio
  insert into tb_anuncio (id_usuario ,nm_anuncio , ds_telefone, ds_valor ,ds_tipo ,ds_descricao )
-			values(1, 'LG velvet 2', '11 957478957', 1200.00 ,'celular','celular bom ');
+			values(1, 'LG velvet 2', '11 957478957', 1200.00 ,'Celular','Celular bom ');
 
-select id_anuncio		id,
-		id_usuario,
-       nm_anuncio		nome,
-       ds_tipo			tipo,
+select id_anuncio		    id,
+		  id_usuario,
+       nm_anuncio		    nome,
+       ds_tipo			    tipo,
        ds_telefone      telefone,
        img_anuncio
   from tb_anuncio;
- where nm_anuncio 		= 'LG velvet'
-   and ds_tipo		    = 'celular';
+ where nm_anuncio 		= 'LG velvet 2'
+   and ds_tipo		    = 'Celular';
      
      
 update tb_anuncio
-set nm_anuncio ='',
-	ds_valor=1000.00 ,
-	ds_tipo	=''	,
-	ds_telefone = '' 
+set nm_anuncio      = '',
+	ds_valor          = 1200.00 ,
+	ds_tipo	          = '' ,
+	ds_telefone       = '' 
 where id_anuncio   
